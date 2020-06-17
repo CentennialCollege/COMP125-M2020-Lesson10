074053d7826bef20183e6ffac2a0eb28f758f39a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-06-17 16:29:36 -0400

    added second function

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,23 @@
 // IIFE -Immediately Ivoked Function Expression
 (function(){
 
+    
+
+
+    // named function
     function Start()
     {
-        console.log("App Started...");
+        console.log('%cApp Started...', "color:white; font-size: 24px;");
+
+        Second();
+    } 
+
+    function Second()
+    {
+        console.log("This is second function");
     }
+
+
 
     window.addEventListener("load", Start);
 
